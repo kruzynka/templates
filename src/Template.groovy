393a88acda92187a7a94bc5773bb95130fb48abc
kruzynka <b.j.kruzynka@gmail.com>
2017-02-16 11:04:12 +0100

    Refactoring for Template.groovy

--- src/Template.groovy
+++ src/Template.groovy
@@ -1,13 +1,21 @@
 class Template {
     String textWithExpressions
 
+    private static final String  EXPRESSION_START = /\$\{/
+    private static final String  EXPRESSION_END = /}/
+
+
     String evaluate(Map<String, String> parameters){
         //kopia tej zmiennej:
         String result = textWithExpressions
         //iterujemy po wszystkich elementach:
         parameters.each { name, value ->
-            result = result.replaceAll(/\$\{/ + name+ /}/, value)
+            result = result.replaceAll(createExpression(name), value)
         }
         return result
+    }
+
+    private String createExpression(String name){
+        EXPRESSION_START + name+ EXPRESSION_END
     }
 }
